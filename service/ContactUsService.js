9df35625f9c7680d4dccf39949e5eaa5a88f91e6
Fabio Codiglioni <fabiocody@icloud.com>
2020-05-08 17:33:33 +0200

    Add contact endpoint to the API

---
+++ service/ContactUsService.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+/**
+ * Send a message via a form
+ *
+ * name String 
+ * email String 
+ * subject String 
+ * message String 
+ * no response value expected for this operation
+ **/
+exports.contactPOST = function(name,email,subject,message) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
