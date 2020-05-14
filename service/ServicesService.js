d3d4aeb9d7cf1b1ff2c514ae4f19ef868c6f7de8
Fabio Codiglioni <fabiocody@icloud.com>
2020-05-14 17:15:46 +0200

    servicesIdPeopleGET

--- service/ServicesService.js
+++ service/ServicesService.js
@@ -40,7 +40,7 @@ exports.servicesGET = function(limit, offset) {
  * id Integer 
  * returns Event
  **/
-exports.servicesIdEventGET = function(id) {
+exports.servicesIdEventGET = function(id) {   // TODO: Remove
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
@@ -89,40 +89,9 @@ exports.servicesIdGET = function(id) {
  * returns List
  **/
 exports.servicesIdPeopleGET = function(id) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "firstName" : "firstName",
-  "lastName" : "lastName",
-  "twitter" : "twitter",
-  "joinDate" : "joinDate",
-  "phoneNumber" : "phoneNumber",
-  "facebook" : "facebook",
-  "serviceDetail" : "serviceDetail",
-  "description" : "description",
-  "id" : 0,
-  "instagram" : "instagram",
-  "picture" : "picture",
-  "email" : "email"
-}, {
-  "firstName" : "firstName",
-  "lastName" : "lastName",
-  "twitter" : "twitter",
-  "joinDate" : "joinDate",
-  "phoneNumber" : "phoneNumber",
-  "facebook" : "facebook",
-  "serviceDetail" : "serviceDetail",
-  "description" : "description",
-  "id" : 0,
-  "instagram" : "instagram",
-  "picture" : "picture",
-  "email" : "email"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+  return database.select('personId', 'description').from(tables.serviceParticipation).where('serviceId', id).then(data => {
+    console.log(data);
+    return data;
   });
 }
 
