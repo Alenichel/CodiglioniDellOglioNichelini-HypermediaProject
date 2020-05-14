5538228e4974f7eb2511d3682e80ac8eb44a9a45
Fabio Codiglioni <fabiocody@icloud.com>
2020-05-14 22:20:24 +0200

    News title, body and media

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -8,8 +8,27 @@ $(document).ready(function() {
     }
     fetch(`/api/v1/news/${id}`).then(response => {
         return response.json();
-    }).then(json => {
-        // TODO: Fill the page
-        console.log(json);
+    }).then(news => {
+        $('#news-title').text(news.title);
+        $('#news-body').append(
+            $(`<img src="${news.media}" alt="" class="text-wrap" width="400">`),
+            $(`<p class="text-justify">`).text(news.body)
+        );
+        if (news.personId != null || news.serviceId != null || news.eventId != null) {
+            let related = $('#news-related');
+            related.append(
+                $('<h3 class="mt-5">').text("Related"),
+            )
+            if (news.personId != null) {
+                console.log("PERSON");
+            }
+            if (news.serviceId != null) {
+                console.log("SERVICE");
+            }
+            if (news.eventId != null) {
+                console.log("EVENT");
+            }
+        }
+        console.log(news);
     });
 })
