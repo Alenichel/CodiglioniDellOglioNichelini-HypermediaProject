77cf02b2a6e398f52991d0eb21897d68791b3bb4
LucaDLL <lucadelloglio@hotmail.it>
2020-05-19 10:50:30 +0200

    Service js refactoring

--- public/assets/js/service.js
+++ public/assets/js/service.js
@@ -2,37 +2,17 @@
 
 function build_st_column(imgURL, id, servicesSize) {
     
-    if (id === 1) { //if initial service: only next button
-        return $('<div class="col-sm-12 col-md-6 col-12  text-center">').append(
-            $('<img class="img-profile rounded" alt="Missing" src="' + imgURL + '">'),
-            $('<div class="row mt-3">').append(
-                $('<div class="col text-right mr-0 mr-md-5">').append(
-                    $(`<a href="/pages/service.html?id=${id+1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-right"></i></a>`)
-                )
+    return $('<div class="col-sm-12 col-md-6 col-12  text-center">').append(
+        $('<img class="img-profile rounded" alt="Missing" src="' + imgURL + '">'),
+        $('<div class="row mt-3">').append(
+            $('<div class="col text-left ml-0 ml-md-5">').append(
+                id !== 1 ? $(`<a href="/pages/service.html?id=${id-1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-left"></i></a>`) : null
+            ),
+            $('<div class="col text-right mr-0 mr-md-5">').append(
+                id !== servicesSize ? $(`<a href="/pages/service.html?id=${id+1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-right"></i></a>`) : null
             )
         )
-    } else if (id === servicesSize) { //if final service: only previous button
-        return $('<div class="col-sm-12 col-md-6 col-12  text-center">').append(
-            $('<img class="img-profile rounded" alt="Missing" src="' + imgURL + '">'),
-            $('<div class="row mt-3">').append(
-                $('<div class="col text-left ml-0 ml-md-5">').append(
-                    $(`<a href="/pages/service.html?id=${id-1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-left"></i></a>`)
-                )
-            )
-        )
-    } else { //if intermediate service: previous and next buttons
-        return $('<div class="col-sm-12 col-md-6 col-12  text-center">').append(
-            $('<img class="img-profile rounded" alt="Missing" src="' + imgURL + '">'),
-            $('<div class="row mt-3">').append(
-                $('<div class="col text-left ml-0 ml-md-5">').append(
-                    $(`<a href="/pages/service.html?id=${id-1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-left"></i></a>`)
-                ),
-                $('<div class="col text-right mr-0 mr-md-5">').append(
-                    $(`<a href="/pages/service.html?id=${id+1}" type="button" class="btn btn-primary btn-sm "><i class="fas fa-chevron-right"></i></a>`)
-                )
-            )
-        )
-    }
+    )
 }
 
 function build_nd_column(name, description, infos) {
@@ -66,25 +46,25 @@ $(document).ready(function() {
     fetch('/api/v1/services').then(response => {
         response.json().then( json => {
             servicesSize = json.length;
+            fetch('/api/v1/services/' + String(id)).then(response => {
+                response.json().then( json => {
+                    
+                    let name = json.name;
+                    let infos = json.infos;
+                    let description = json.description;
+                    //TODO let presentedInEvent
+                    let picture = json.pictures[0];
+        
+                    $('#service-info-row').append(
+                        build_st_column(picture, id, servicesSize),
+                        build_nd_column(name, description, infos)
+                    );
+                    $('#service-offeredBy-row').append(
+                        build_nd_row()
+                    );
+                })
+            })
         })
     })
 
-    fetch('/api/v1/services/' + String(id)).then(response => {
-        response.json().then( json => {
-            
-            let name = json.name;
-            let infos = json.infos;
-            let description = json.description;
-            //TODO let presentedInEvent
-            let picture = json.pictures[0];
-
-            $('#service-info-row').append(
-                build_st_column(picture, id, servicesSize),
-                build_nd_column(name, description, infos)
-            );
-            $('#service-offeredBy-row').append(
-                build_nd_row()
-            );
-        })
-    })
 })
