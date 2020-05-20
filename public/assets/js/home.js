c6551538845cbefa42e723cf4f570b628ba2466e
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-05-20 17:07:18 +0200

    very proof of concept home page

---
+++ public/assets/js/home.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function build_person_row(name, img, i){
+
+}
+
+$(document).ready(function() {
+
+    /*fetch('/api/v1/people').then(response => {
+        response.json().then( json => {
+            for (let s of json){
+                let fs = s.firstName
+                let img = s.picture
+                $('#people-row').append(build_person_row(fs, img));
+            }
+        })
+    })*/
+})
