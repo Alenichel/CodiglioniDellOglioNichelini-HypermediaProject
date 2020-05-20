45ec9a4d27a6f583cc238a86613e3cf687fe2216
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-05-20 22:52:55 +0200

    improved homepage

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,18 +1,35 @@
 "use strict";
 
-function build_person_row(name, img, i){
-
+function build_person_row(name, img, join_date, i){
+    let person_row = $('<div class="row">')
+    person_row.append(
+        $('<div class="col col-md-auto align-self-center">').append(
+            $(`<img class="img-profile rounded-circle" alt="Missing img" src="${img}">`)
+        ),
+        $('<div class="col col-md-auto align-self-center">').append(
+            $(`<p>${name}<br>Joined ${join_date}</p>`)
+        )
+    )
+    return person_row;
 }
 
 $(document).ready(function() {
 
-    /*fetch('/api/v1/people').then(response => {
+    fetch('/api/v1/people').then(response => {
         response.json().then( json => {
+            let i = 0;
             for (let s of json){
                 let fs = s.firstName
                 let img = s.picture
-                $('#people-row').append(build_person_row(fs, img));
+                let join_date = s.joinDate.
+                if (i===3) {
+                    return;
+                }
+                $('#last_joined').append(
+                    build_person_row(fs, img,join_date, i)
+                );
+                i++;
             }
         })
-    })*/
+    })
 })
