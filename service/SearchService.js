0c86824a5aa8cf5670d687ee4e50f22e11b7e2fb
Fabio Codiglioni <fabiocody@icloud.com>
2020-05-22 16:21:41 +0200

    SearchService.js

--- service/SearchService.js
+++ service/SearchService.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const databaseService = require('./database');
+let database = databaseService.database
+let tables = databaseService.tables
+
 
 /**
  * Perform a search in the entire site
@@ -8,22 +12,53 @@
  * returns List
  **/
 exports.searchGET = function(query) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "name" : "name",
-  "media" : "media",
-  "type" : "type"
-}, {
-  "name" : "name",
-  "media" : "media",
-  "type" : "type"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+  return new Promise(async (resolve, reject) => {
+    let people = await database.select('id', 'firstName', 'lastName', 'picture').from(tables.person);
+    people = people.map(p => {
+      return {
+        id: p.id,
+        name: p.firstName + ' ' + p.lastName,
+        media: p.picture,
+        type: 'person'
+      };
+    });
+    let events = await database.select('id', 'name', 'picture').from(tables.event);
+    events = events.map(e => {
+      return {
+        id: e.id,
+        name: e.name,
+        media: e.picture,
+        type: 'event'
+      };
+    });
+    let services = await database.select('id', 'name').from(tables.service);
+    for (let s of services) {
+      let pictures = await database.select('filename').from(tables.servicePicture).where('serviceId', s.id).limit(1);
+      s.picture = pictures[0].filename;
     }
+    services = services.map(s => {
+      return {
+        id: s.id,
+        name: s.name,
+        media: s.picture,
+        type: 'service'
+      };
+    });
+    let news = await database.select('id', 'title', 'media').from(tables.news);
+    news = news.map(n => {
+      return {
+        id: n.id,
+        name: n.title,
+        media: n.media,
+        type: 'news'
+      };
+    })
+    let data = people.concat(events).concat(services).concat(news);
+    console.log(query);
+    data = data.filter(x => {
+      return x.name.includes(query);
+    })
+    resolve(data);
   });
 }
 
